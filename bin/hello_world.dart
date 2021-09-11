aa2fa2af3dc6ac1fe734639a330268278f78f815
tamiltechguru <tamiltechguru2@gmail.com>
2021-09-11 19:15:44 +0530

    first commit

---
+++ bin/hello_world.dart
@@ -0,0 +1,10 @@
+import 'package:characters/characters.dart';
+
+void main(List<String> arguments) {
+  const family = '👨‍👩‍👧‍👦';
+  print(family.length);
+  print(family.codeUnits);
+  print(family.runes);
+  print(family.characters.length);
+  print(family.length);
+}
