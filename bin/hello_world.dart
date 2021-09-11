78b90c8edc446f809ffa94749878faabbec1979d
tamiltechguru <tamiltechguru2@gmail.com>
2021-09-11 19:48:21 +0530

    ch3_chl_1

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -1,8 +1,18 @@
 void main(List<String> arguments) {
-  const firstName = 'Kannan';
-  const lastName = 'Ramaswamy';
-  const fullName = firstName + ' ' + lastName;
-  print(fullName);
-  const myDetails = 'Hello, my name is $fullName';
-  print(myDetails);
+  // Teachers challenge
+  // attendance is worth 20%
+  // homework is worth 30%
+  // exam is worth 50%.
+  // student got 90 attendance, 80 homework and 94 exam.
+  // Calculate her grade as an integer percentage rounded down.
+  const attendanceWeitage = 0.2;
+  const homeworkWeitage = 0.3;
+  const examWeitage = 0.5;
+  const studentAttendacne = 90;
+  const studentHomework = 80;
+  const studentExam = 94;
+  const grade = attendanceWeitage * studentAttendacne +
+      homeworkWeitage * studentHomework +
+      examWeitage * studentExam;
+  print(grade.round());
 }
